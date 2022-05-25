ed585b6e18f8b26249f5f76339b71f6eb0cb4266
dharmin_makrubiya <dharminmakrubiya@hotmail.com>
2022-05-25 15:32:49 +0530

    show categories list in frontend

--- wp-content/themes/masonic/functions.php
+++ wp-content/themes/masonic/functions.php
@@ -727,3 +727,31 @@ function create_companies()
 
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// $args = array(
+//    'child_of'            => 0,
+//    'current_category'    => 0,
+//    'depth'               => 0,
+//    'echo'                => 1,
+//    'exclude'             => '',
+//    'exclude_tree'        => '',
+//    'feed'                => '',
+//    'feed_image'          => '',
+//    'feed_type'           => '',
+//    'hide_empty'          => 0,
+//    'hide_title_if_empty' => false,
+//    'hierarchical'        => true,
+//    'order'               => 'ASC',
+//    'orderby'             => 'name',
+//    'separator'           => '<br />',
+//    'show_count'          => 0,
+//    'show_option_all'     => '',
+//    'show_option_none'    => __( 'No categories' ),
+//    'style'               => 'list',
+//    'taxonomy'            => 'category',
+//    'title_li'            => __( 'Categories' ),
+//    'use_desc_for_title'  => 1,
+// );
+
+// var_dump( wp_list_categories($args) );
